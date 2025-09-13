16333258cbd04adabf1edabc06d434b8b7e9dbec
Aleksandr Gavrik <a.gavrik@mercuryo.io>
2025-09-13 16:20:11 +0300

    chore(bot): remove excluded IPs note from access creation message

--- fab/bot/handlers.py
+++ fab/bot/handlers.py
@@ -192,16 +192,7 @@ async def handle_add_access(query, user_id: int, chat_id: int) -> None:
         # Generate dynamic link
         access_url = f"{config.site_url}/{session.token}"
         
-        # Inform user if IP is excluded from RabbitMQ publishing (always open policy)
-        excluded_note = ""
-        if config.exclude_ips:
-            excluded_note = (
-                "\n\n" + i18n.get_text(
-                    "bot.excluded_ips_note",
-                    ips=", ".join(config.exclude_ips)
-                )
-            )
-        response_text = i18n.get_text("bot.access_link_created", link=access_url) + excluded_note
+        response_text = i18n.get_text("bot.access_link_created", link=access_url)
         
         keyboard = [
             [InlineKeyboardButton(i18n.get_text("bot.main_menu"), callback_data="main_menu")]
